3277abe68e4b8f04b5770a96941edc8e6c421fb8
matnordlund <mattias@xanthos.se>
2024-10-14 09:11:15 +0200

    Update healthcheck-script.sh

--- healthcheck-script.sh
+++ healthcheck-script.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 CONNECTION_NAME="sase_logging_net"  # Name of the strongSwan connection to monitor
+IKE_NAME="sase_logging_tunnel" # Name of the strongSwan IKE 
+
 CHILD_SA_STATUS=$(swanctl --list-sas | grep -A 5 "${CONNECTION_NAME}" | grep "INSTALLED")
     
 if [[ -n "$CHILD_SA_STATUS" ]]; then
-      echo "$(date): Child SAs for connection '${CONNECTION_NAME}' are UP" 
-      exit 0
+    echo "$(date): Child SAs for connection '${CONNECTION_NAME}' are UP" >&1  # Log to stdout for Docker logs
+    exit 0
 else
-      echo "$(date): ERROR: Child SAs for connection '${CONNECTION_NAME}' are DOWN" 
-      exit 1
-# Optionally: Restart the connection if the child SA is down
-# echo "Restarting connection '${CONNECTION_NAME}'" 
-# swanctl --terminate --ike ${CONNECTION_NAME}
-# swanctl --initiate --ike ${CONNECTION_NAME}
+    echo "$(date): ERROR: Child SAs for connection '${CONNECTION_NAME}' are DOWN" >&1  # Log to stdout for Docker logs
+    echo "$(date): Trying to restart the connection '${IKE_NAME}'" >&1
+    swanctl --terminate --ike ${IKE_NAME}
+    swanctl --initiate --ike ${IKE_NAME}
+    exit 1       
 fi
